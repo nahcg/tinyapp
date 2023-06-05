1213f56cba429d485b3050e498ed4c15bf607743
nahcg <glor.chan@mail.utoronto.ca>
2023-06-05 12:16:37 -0400

    add route for /urls page

--- express_server.js
+++ express_server.js
@@ -28,3 +28,9 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// route for /urls to render urls ejs template in views folder
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
