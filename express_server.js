62cff764a9471a233099dffc2c748abd746a138e
nahcg <glor.chan@mail.utoronto.ca>
2023-06-05 10:22:39 -0400

    set ejs as view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set ejs as the view engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,4 +22,9 @@ app.listen(PORT, () => {
 // add additional endpoints
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+// sending HTML in response
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
