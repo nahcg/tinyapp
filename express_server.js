32ca056c751fa37e9c75f4c24fdfad54d1970be6
nahcg <glor.chan@mail.utoronto.ca>
2023-06-06 21:08:15 -0400

    add generateRandomString func

--- express_server.js
+++ express_server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let s = '';
+  s += Math.random().toString(36).slice(2);
+  return s.slice(0, 6);
+}
+
 // set ejs as the view engine
 app.set("view engine", "ejs");
 
@@ -9,6 +15,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// middleware from body-parser library that converts the request body from a Buffer into string that can be read
+app.use(express.urlencoded({ extended: true }));
 
 // registers a handler on the root path, "/".
 app.get("/", (req, res) => {
@@ -35,8 +44,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// add GET route to render the urls_new.ejs template
+// urls_new has form whose method is set to POST
+// The form has one named input, with the name attribute set to longURL
+// when the form is submitted, it will make a request to POST /urls, and the body will contain one URL-encoded name-value pair with the name longURL
+// data in the input field will be avaialbe to us in the req.body.longURL variable, which we can store in our urlDatabase object
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // add route to render urls_show.ejs template
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+// route that logs the request body and gives a dummy response
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
